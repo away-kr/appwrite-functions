886bb290fe30e2765214ac96201701035b55f5ad
away-kr <168698379+away-kr@users.noreply.github.com>
2024-05-02 09:24:45 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -14,17 +14,8 @@ def main(context):
     #     .set_key(os.environ["APPWRITE_API_KEY"])
     # )
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
-
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
-
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
+    # You can log messages to the consol
+    return context.res.send("Hello, World!")
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
